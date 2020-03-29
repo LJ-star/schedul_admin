f75ec6ff2fa691fd0ff7c687e7db043ebe786d0a
LJ_star <lukejin163@protonmail.com>
2020-03-29 09:20:09 -0700

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -864,3 +864,22 @@ function mobileMenu() {
 // });
 //======Multiselect options end
 
+window.blockMenuHeaderScroll = false;
+$(window).on('touchstart', function(e)
+{
+    if ($(e.target).closest('#mobileMenuHeader').length == 1)
+    {
+        blockMenuHeaderScroll = true;
+    }
+});
+$(window).on('touchend', function()
+{
+    blockMenuHeaderScroll = false;
+});
+$(window).on('touchmove', function(e)
+{
+    if (blockMenuHeaderScroll)
+    {
+        e.preventDefault();
+    }
+});
